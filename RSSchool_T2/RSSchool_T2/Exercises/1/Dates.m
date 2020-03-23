4c9fbc82bb7776dc9a21910b689834a54af001e7
Vil Smith <vi-ly@mail.ru>
2020-03-23 23:13:16 +0300

    add: code to ex1 and ex2

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,34 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+  //  formatter.dateFormat = @"d MMMM, EEEE";
+    
+    formatter.dateFormat = @"dd.MM.yyyy";
+    
+    NSString *dateString = [NSString stringWithFormat: @"%@, %@, %@", day, month, year];
+    
+    NSDate *dateDate = [formatter dateFromString:dateString];
+    
+    NSDateFormatter *formatter2 = [[NSDateFormatter alloc] init];
+    formatter2.dateFormat = @"d MMMM, EEEE";
+    formatter2.locale = [NSLocale localeWithLocaleIdentifier:@"ru_BY"];
+    
+    NSString *stringDate = [formatter2 stringFromDate:dateDate];
+    
+    if (stringDate == (NSString*)nil){
+        return @"Такого дня не существует";
+    }
+ 
+    return stringDate;
+    
+    
+    
+    
+    //return @"";
 }
 
 @end
--- RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
+++ RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
@@ -3,6 +3,80 @@
 @implementation TimeConverter
 // Complete the following function
 - (NSString*)convertFromHours:(NSString*)hours minutes:(NSString*)minutes {
-    return @"";
+    
+    NSArray *array=[[NSArray alloc]initWithObjects:@"o' clock", @"one", @"two", @"three", @"four", @"five", @"six", @"seven", @"eight", @"nine", @"ten", @"eleven", @"twelve", @"thirteen", @"fourteen", @"quarter", @"sixteen", @"seventeen", @"eighteen", @"nineteen", @"twenty", @"thirty", @"forty", @"fifty", @"half", nil];
+
+    NSInteger min = [minutes integerValue];
+    NSInteger hour = [hours integerValue];
+    NSString *rezultMin = @"";
+    
+    NSString *rezultHour = @"";
+    NSString *rezult = @"";
+
+      
+      for (NSUInteger i = 0; i < array.count; i++) {
+          
+          if (0 < min && min < 21 && hour == i) {
+              if (min != 15){
+             rezultMin =  [NSString stringWithFormat: @"%@ minutes", [array objectAtIndex:min]];
+                  rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex: i ]];
+                  rezult  = [NSString stringWithFormat:@"%@ past %@",rezultMin, rezultHour];
+          }
+              if (min == 15 && hour == i) {
+                  rezultMin =  [NSString stringWithFormat: @"%@", [array objectAtIndex:min]];
+                  rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex: i ]];
+                  rezult  = [NSString stringWithFormat:@"%@ past %@",rezultMin, rezultHour];
+              }
+          }
+          if  (20 < min && min < 30) {
+              
+               NSUInteger simleNumbersKey =  min % 10;
+               NSString *simleNumbersName = [NSString stringWithFormat:@"%@",[array objectAtIndex:simleNumbersKey]];
+               NSString *tensKey = [array objectAtIndex:20];
+               rezultMin = [NSString stringWithFormat:@"%@ %@ minutes", tensKey, simleNumbersName];
+               rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex: i ]];
+               rezult  = [NSString stringWithFormat:@"%@ past %@",rezultMin, rezultHour];
+              
+          }
+          if (30 < min && min < 40 && hour == i) {
+              NSString *tensKey = [array objectAtIndex:20];
+              NSUInteger simleNumbersKey = (60 - min) % 10;
+              NSString *simleNumbersName = [NSString stringWithFormat:@"%@",[array objectAtIndex:simleNumbersKey]];
+              rezultMin = [NSString stringWithFormat:@"%@ %@ minutes", tensKey, simleNumbersName];
+              rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex: i + 1 ]];
+              rezult = [NSString stringWithFormat:@"%@ to %@", rezultMin, rezultHour];
+          }
+          
+          if (39 < min && min<60 && min != 45 && hour == i) {
+          
+          NSUInteger simleNumbersKey =  60 - min;
+          NSString *simleNumbersName = [NSString stringWithFormat:@"%@",[array objectAtIndex:simleNumbersKey]];
+          rezultMin = [NSString stringWithFormat:@"%@ minutes", simleNumbersName];
+          rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex: i + 1 ]];
+          rezult = [NSString stringWithFormat:@"%@ to %@", rezultMin, rezultHour];
+          }
+        
+          if (min == 30 && hour==i ) {
+              rezultMin = [NSString stringWithFormat:@"%@", [array objectAtIndex:24]];
+              rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex: i]];
+              rezult = [NSString stringWithFormat:@"%@ past %@",rezultMin, rezultHour];
+          }
+      
+          if (min == 45 && hour == i) {
+              rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex: i + 1 ]];
+              rezultMin = [NSString stringWithFormat:@"%@ ", [array objectAtIndex: 15]];
+              rezult = [NSString stringWithFormat:@"%@to %@",rezultMin, rezultHour];
+          }
+    
+          if (min == 0 && hour == i) {
+              rezultHour = [NSString stringWithFormat:@"%@", [array objectAtIndex:i]];
+              rezult = [NSString stringWithFormat:@"%@ o' clock", rezultHour];
+          }
+          if (min > 59){
+              rezult = @"";
+          }
+      }
+    
+    return rezult;
 }
 @end
